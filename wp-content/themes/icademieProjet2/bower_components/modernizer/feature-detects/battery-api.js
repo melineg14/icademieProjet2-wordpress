0dede8c9c5932b9d95a747ddd706f57df0dc353a
melineg14 <melineg14@gmail>
2020-08-15 08:23:46 +0200

    initial commit

---
+++ wp-content/themes/icademieProjet2/bower_components/modernizer/feature-detects/battery-api.js
@@ -0,0 +1,9 @@
+
+// Battery API
+// https://developer.mozilla.org/en/DOM/window.navigator.mozBattery
+// By: Paul Sayre
+
+Modernizr.addTest(
+    'battery',
+    !!Modernizr.prefixed('battery', navigator)
+);
---
+++ wp-content/themes/icademieProjet2/bower_components/modernizer/feature-detects/workers-sharedworkers.js
@@ -0,0 +1,5 @@
+Modernizr.addTest(
+    'sharedworkers', function () {
+        return !!window.SharedWorker;
+    }
+);
